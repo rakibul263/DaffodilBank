54de0293e2dac61a28bb49b6364006148f7a1f6a
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-11-11 03:48:56 +0600

    v5

--- src/main/java/com/jmc/diu/daffodilbank/Controllers/LoginController.java
+++ src/main/java/com/jmc/diu/daffodilbank/Controllers/LoginController.java
@@ -1,15 +1,25 @@
 package com.jmc.diu.daffodilbank.Controllers;
 
+import com.jmc.diu.daffodilbank.Models.Model;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public Text account_number_lbl;
     public TextField account_number_fld;
     public Text password_lbl;
     public TextField password_fld;
     public Button login_btn;
     public Label error_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_btn.setOnAction(event -> Model.getInstance().getViewFactory().showClientWindow());
+    }
 }
--- src/main/java/com/jmc/diu/daffodilbank/Models/Model.java
+++ src/main/java/com/jmc/diu/daffodilbank/Models/Model.java
@@ -1,4 +1,22 @@
 package com.jmc.diu.daffodilbank.Models;
-
+import com.jmc.diu.daffodilbank.Views.ViewFactory;
 public class Model {
+        private static Model model;
+        private final ViewFactory viewFactory;
+
+    private Model(){
+        this.viewFactory = new ViewFactory();
+    }
+
+    public static synchronized Model getInstance(){
+        if(model == null){
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory(){
+        return viewFactory;
+    }
+
 }
