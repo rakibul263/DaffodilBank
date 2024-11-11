9c38d510aefe7477943c84c7c37b81d4f33d8180
Md Rakibul Hasan <rakibulhasanshuvo206@gmail.com>
2024-11-11 21:58:59 +0600

    admin menu

---
+++ src/main/java/com/jmc/diu/daffodilbank/Controllers/Admin/AdminMenuController.java
@@ -0,0 +1,17 @@
+package com.jmc.diu.daffodilbank.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+    public Button create_client;
+    public Button client_btn;
+    public Button deposite_btn;
+    public Button logout_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+}
